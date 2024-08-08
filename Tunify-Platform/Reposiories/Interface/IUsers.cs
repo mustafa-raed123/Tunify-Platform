15ae5dbc9fb664b7ae6a70a9790273737e29d355
mura123+ <mwsym1898@gmail.com>
2024-08-08 10:40:40 +0300

    add

---
+++ Tunify-Platform/Reposiories/Interface/IUsers.cs
@@ -0,0 +1,13 @@
+﻿using Tunify_Platform.Data.Models;
+
+namespace Tunify_Platform.Reposiories.Interface
+{
+    public interface IUsers
+    {
+        public Task<List<User>> GetAllUsers();
+        public Task<User> UpdateUser(int Id  , User user);
+        public Task<int> DeleteUser(int Id);
+        public Task<User> GetUserById(Task<int> id);
+        public Task<User> CreateUser(User user);
+    }
+}
